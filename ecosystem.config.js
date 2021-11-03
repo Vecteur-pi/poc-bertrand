8961d41700c9f55db6ec0f34c3792d6df103975b
Bertrand <bertrandpi.bp@gmail.com>
2021-11-04 00:29:29 +0100

    modif

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -16,7 +16,7 @@ module.exports = {
       host: '212.83.167.103',
       ref: 'origin/master',
       repo: 'https://github.com/Vecteur-pi/poc-bertrand.git',
-      path: '/home/ubuntu/scaleway/source',
+      path: '/home/ubuntu/scaleway',
       'pre-deploy-local': '',
       'post-deploy':
         'npm install && pm2 reload ecosystem.config.js --env production',
